780b57d7e17662ef05b59640b69755b5bedc368d
Michi <michi@example.com>
2023-10-03 11:58:10 -0400

    get characters route

--- server/server.js
+++ server/server.js
@@ -5,5 +5,9 @@ const PORT = 3001;
 
 const app = express();
 
+app.get('/api/characters', (req, res) => res.json(planetsData));
+
 app.get('/api/planets', (req, res) => res.json(planetsData));
-app.listen(PORT)
+
+
+app.listen(PORT);
